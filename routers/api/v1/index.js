abb1fc15be445b4c0afced0a9163444a6adbe832
BAEKSEONGYUN <baek@nsmarts.co.kr>
2024-12-20 16:44:35 +0900

    코드 정리리

--- routers/api/v1/index.js
+++ routers/api/v1/index.js
@@ -1,8 +1,10 @@
-let express = require('express');
-let router = express.Router();
+let express = require('express'); // express 모듈 가져오기
+let router = express.Router(); // express 라우터 생성
 
-let chat = require('./chat');
+let chat = require('./chat'); // chat 모듈 가져오기
 
-router.use('/chat', chat)
+// '/chat' 경로로 들어오는 요청을 chat 모듈로 위임
+router.use('/chat', chat);
 
+// 라우터를 모듈로 내보내기
 module.exports = router;
